d1f0844f22638926faac510818edce46860048f3
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-08-07 00:55:37 +0100

    Refactor : mobile-first design adding responsive

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,64 @@
+import Logo from "../assets/logo.svg";
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
+
+const navItems = [
+  { name: "Accueil", href: "#" },
+  { name: "Services", href: "#" },
+  { name: "À propos", href: "#" },
+  { name: "Contact", href: "#" },
+  { name: "Gallery", href: "#" },
+];
+const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <nav className="container mx-auto flex items-center justify-between p-6 shadow-md">
+      <div>
+        <img src={Logo} alt="" />
+      </div>
+      <div className="hidden md:block">
+        <ul>
+          {navItems.map((item) => (
+            <li key={item.name} className="inline-block mx-4">
+              <a href={item.href} className="text-gray-700 hover:text-gray-900">
+                {item.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="md:hidden">
+        <button
+          onClick={toggleMenu}
+          className="text-gray-700 hover:text-gray-900"
+          aria-label="Open navigation menu"
+          title="Open navigation menu"
+        >
+          {isOpen ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
+        </button>
+        {isOpen && (
+          <div className="absolute top-0 left-0 h-screen w-[50%] bg-white shadow-lg z-50 transition-all duration-300 md:hidden">
+            <ul className="flex flex-col p-6">
+              {navItems.map((item) => (
+                <li key={item.name} className="mb-4">
+                  <a
+                    href={item.href}
+                    className="text-gray-700 hover:text-gray-900 text-lg"
+                    onClick={() => setIsOpen(false)}
+                  >
+                    {item.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
