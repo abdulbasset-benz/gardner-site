178d6be9b1eb484df32db6f9a7a695d26db3e707
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-08-08 13:24:52 +0100

    Add the experience section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./index.css";
 import Hero from "./components/Hero";
+import Experience from "./components/Experience";
 
 const App = () => {
   return (
     <div>
       <Hero />
+      <Experience />
     </div>
   );
 };
