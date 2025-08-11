09e67099335a8eb75d3cb1be0a50cb8dc1c09ecf
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-08-11 13:32:09 +0100

    Import Steps, CTA and footer components

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,20 @@ import Navigation from "./components/Navigation";
 import Hero from "./components/Hero";
 import Experience from "./components/Experience";
 import Services from "./components/Services";
+import Steps from "./components/Steps";
+import CallToAction from "./components/CallToAction";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div>
+    <div className="">
       <Navigation />
       <Hero />
       <Experience />
       <Services />
+      <Steps />
+      <CallToAction />
+      <Footer />
     </div>
   );
 };
