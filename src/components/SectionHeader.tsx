868db69a0eb586a540b86928c91e1c216527e9a2
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-08-11 13:40:45 +0100

    minor changes

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -2,7 +2,7 @@ import type { SectionHeaderProps } from "../types/types";
 const SectionHeader = ({title, description} : SectionHeaderProps) => {
   return (
     <div className="mb-8 text-center">
-      <h2 className="text-4xl capitalize font-poppins font-bold text-primary mb-3">{title}</h2>
+      <h2 className="text-4xl md:text-5xl capitalize font-poppins font-bold text-primary mb-3">{title}</h2>
       {description && (
         <p className="text-sm text-gray-600 font-poppins max-w-sm mx-auto leading-relaxed">
           {description}
